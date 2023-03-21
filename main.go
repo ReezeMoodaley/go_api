edb48c93f9c0eed55fd4dbe4657025a45235892a
Reeze Moodaley <reeze@iprocess.co.za>
2023-03-21 08:01:18 +0200

    WeatherAPI integration works, also removed some useless methods

--- main.go
+++ main.go
@@ -6,46 +6,52 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
 
-type Article struct {
-	Id      string `json:Id`
-	Title   string `json:"Title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
+type WeatherResponse struct {
+	Location struct {
+		Name      string  `json:"name"`
+		Region    string  `json:"region"`
+		Country   string  `json:"country"`
+		Lat       float64 `json:"lat"`
+		Lon       float64 `json:"lon"`
+		TZID      string  `json:"tz_id"`
+		Localtime string  `json:"localtime"`
+	} `json:"location"`
+	Current struct {
+		TempC      float64 `json:"temp_c"`
+		FeelsLikeC float64 `json:"feelslike_c"`
+		Condition  struct {
+			Text string `json:"text"`
+			Icon string `json:"icon"`
+		} `json:"condition"`
+		Humidity   int     `json:"humidity"`
+		WindMph    float64 `json:"wind_mph"`
+		PressureIn float64 `json:"pressure_in"`
+		UV         float64 `json:"uv"`
+	} `json:"current"`
 }
 
-// using this article slice to simulate a DB
-var Articles []Article
-
-//if we want to send messages back to the client hitting our endpoints we need to make
+// if we want to send messages back to the client hitting our endpoints we need to make
 // use of the http.ResponseWriter
+var client *http.Client
+
+//ENDPOINTS
+
+func weatherPage(w http.ResponseWriter, r *http.Request) {
+	println("Port Elizabeth Weather -> End Point")
+	fmt.Fprintf(w, "Port Elizabeth Weather -> End Point")
+	GetWeatherInfo()
+
+}
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	println("Endpoint hit! -> Home Page")
 	fmt.Fprintf(w, "Welcome to the Home Page!")
-
-}
-
-func returnAllArticles(w http.ResponseWriter, r *http.Request) {
-	println("Endpoint hit! -> return all articles")
-	json.NewEncoder(w).Encode(Articles)
-}
-
-func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
-
-	vars := mux.Vars(r)
-	key := vars["id"]
-	fmt.Fprintf(w, "Welcome to Article %v", key)
-
-	for _, article := range Articles {
-		if article.Id == key {
-			json.NewEncoder(w).Encode(article)
-		}
-	}
 
 }
 
@@ -54,19 +60,53 @@ func indexPage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the Index page Regulars")
 }
 
+//END OF ENDPOINTS
+
+func GetWeatherInfo() {
+
+	url := "http://api.weatherapi.com/v1/current.json?key=6f811e8ca8a94ee1b6b100326231903&q=London&aqi=no"
+
+	var weatherResponse WeatherResponse
+
+	err := GetJson(url, &weatherResponse)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Current Weather in %v: \n\n %v", weatherResponse.Location.Name, weatherResponse.Current.TempC)
+	}
+
+}
+
+// GENERIC METHOD USED FOR PARSING THE JSON DATA WE GET FROM OUR GET REQUESTS
+func GetJson(url string, target interface{}) error {
+
+	resp, err := client.Get(url)
+
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	return json.NewDecoder(resp.Body).Decode(target)
+}
+
+// USED FOR THE HANDLING OF REQUESTS
 func handleRequests() {
 
 	myRouter := mux.NewRouter().StrictSlash(true)
 
 	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/index", indexPage)
-	myRouter.HandleFunc("/articles", returnAllArticles)
-	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+	myRouter.HandleFunc("/weather", weatherPage)
 
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
 func main() {
+
+	client = &http.Client{Timeout: 10 * time.Second}
 	//importing of the ENV variables
 	err := godotenv.Load()
 	if err != nil {
@@ -78,9 +118,5 @@ func main() {
 
 	fmt.Println("Rest API v2.0 - Mux Routers")
 
-	Articles = []Article{
-		Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
-	}
 	handleRequests()
 }
